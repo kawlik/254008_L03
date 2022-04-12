82408238368fee855fb44710e21656897e639f19
kawlik <kpawlik1103@gmail.com>
2022-04-12 16:54:38 +0200

    Dodaj pliki projektów.

---
+++ Problem.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Parallelism
+{
+    public class ThreadedWork
+    {
+        //  threaded work storage
+        public List<long>[] results;
+
+        //  initialize threaded work mutex
+        public ThreadedWork(int size)
+        {
+            this.results = new List<long>[size];
+        }
+
+        //  save work on results list
+        public void SetWork(int index, List<long> partialResult)
+        {
+            this.results[index] = partialResult;
+        }
+    }
+
+    public class Result
+    {
+        public double ElapsedTime;
+        public long PrimesCount;
+
+        public Result(double ElapsedTime, long PrimesCount)
+        {
+            this.ElapsedTime = ElapsedTime;
+            this.PrimesCount = PrimesCount;
+        }
+    }
+
+    public class Problem
+    {
+        private long max;
+        private long min;
+
+        public Problem(long max, long min = 2)
+        {
+            this.max = max;
+            this.min = min;
+        }
+
+        /*  utilities
+        /*   *   *   *   *   *   *   *   *   */
+
+        public static bool IsPrimeNumber(long number)
+        {
+            //  initial cond
+            if(number < 2) return false;
+
+            //  set limit
+            long limit = (long)Math.Sqrt(number);
+
+            //  calc
+            for (long i = 2; i <= limit; i++)
+            {
+                //  breaking cond
+                if(number % i == 0) return false;
+            }
+
+            //  return true
+            return true;
+        }
+
+        public static List<long> GetPrimeNumbersInRange(long minimum, long maximum)
+        {
+            //  set initial data
+            List<long> primes = new List<long>();
+
+            //  calc
+            for (long i = minimum; i <= maximum; i++)
+            {
+                //  test current value
+                if(IsPrimeNumber(i)) primes.Add(i);
+            }
+
+            //  return result
+            return primes;
+        }
+
+        private static async Task<List<long>> GetPrimeNumbersInRangeAsync(long minimum, long maximum)
+        {
+            //  set initial 
+            List<long> primes = new List<long>();
+
+            //  count offsets
+            var count = maximum - minimum + 1;
+
+            //  return async task
+            return await Task.Factory.StartNew(() =>
+            {
+                //  calc
+                for (long i = minimum; i <= maximum; i++)
+                {
+                    //  test current value
+                    if (IsPrimeNumber(i)) primes.Add(i);
+                }
+
+                //  return result
+                return primes;
+            });
+        }
+
+        /*  solutions
+        /*   *   *   *   *   *   *   *   *   */
+
+        //  basic version
+        public Result GetViaOneThread()
+        {
+            //  watch start
+            var watch = Stopwatch.StartNew();
+
+            /*  start
+            /*   *   *   *   *   *   *   *   */
+
+
+            //  set result
+            List<long> primes = Problem.GetPrimeNumbersInRange(this.min, this.max);
+
+
+            /*  stop
+            /*   *   *   *   *   *   *   *   */
+
+            //  watch stop
+            watch.Stop();
+
+            //  return result
+            return new Result(watch.Elapsed.TotalMilliseconds, primes.Count);
+        }
+
+        //  Parallel version
+        public Result GetViaParallel(int numParallel)
+        {
+            //  watch start
+            var watch = Stopwatch.StartNew();
+
+            /*  start
+            /*   *   *   *   *   *   *   *   */
+
+            //  init parital results list
+
+            var primes = new List<long>[numParallel];
+
+            //  calc offset
+            long offset = (this.max - this.min) / numParallel;
+
+            //  execute in parallel
+            Parallel.For(0, numParallel, i => primes[i] = Problem.GetPrimeNumbersInRange(i * offset, i < numParallel ? (i + 1) * offset - 1 : this.max));
+
+            /*  stop
+            /*   *   *   *   *   *   *   *   */
+
+            //  watch stop
+            watch.Stop();
+
+            //  return result
+            return new Result(watch.Elapsed.TotalMilliseconds, primes.Sum(partial => partial.Count));
+        }
+
+        //  Tasks version
+        public async Task<Result> GetViaTasks(int numTasks)
+        {
+            //  watch start
+            var watch = Stopwatch.StartNew();
+
+            /*  start
+            /*   *   *   *   *   *   *   *   */
+
+            //  init parital results list
+            var tasks = new Task<List<long>>[numTasks];
+
+            //  calc offset
+            long offset = (long)((this.max - this.min) / numTasks);
+
+            //  exec
+            for (int i = 0; i < numTasks; i++)
+            {
+                tasks[i] = Problem.GetPrimeNumbersInRangeAsync(i * offset, i < numTasks ? (i + 1) * offset - 1 : this.max);
+            }
+
+            //  wait for result
+            var primes = await Task.WhenAll(tasks);
+
+
+            /*  stop
+            /*   *   *   *   *   *   *   *   */
+
+            //  watch stop
+            watch.Stop();
+
+            //  return result
+            return new Result(watch.Elapsed.TotalMilliseconds, primes.Sum(partial => partial.Count));
+        }
+
+        //  Thread version
+        public Result GetViaThreads(int numThreads)
+        {
+            //  watch start
+            var watch = Stopwatch.StartNew();
+
+            /*  start
+            /*   *   *   *   *   *   *   *   */
+
+
+            //  init parital results list
+            Thread[] threads = new Thread[numThreads];
+            ThreadedWork res = new ThreadedWork(numThreads);
+
+            //  calc offset
+            long offset = (this.max - this.min) / numThreads;
+
+            //  exec
+            for (int i = 0; i < numThreads; i++)
+            {
+                int index = i;
+
+                threads[i] = new Thread(() => res.SetWork(index, Problem.GetPrimeNumbersInRange(index * offset, index < numThreads ? (index + 1) * offset - 1: this.max)));
+            }
+
+            //  start threads
+            for (int i = 0; i < numThreads; i++) threads[i].Start();
+            for (int i = 0; i < numThreads; i++) threads[i].Join();
+
+
+            /*  stop
+            /*   *   *   *   *   *   *   *   */
+
+            //  watch stop
+            watch.Stop();
+
+            //  return result
+            return new Result(watch.Elapsed.TotalMilliseconds, res.results.Sum(partial => partial.Count));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallelism
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //  set problem
+            Problem problem = new Problem(10000000);
+
+
+            //  get solutions
+            var result1 = problem.GetViaOneThread();
+            var result2 = problem.GetViaParallel(4);
+            var result3 = problem.GetViaParallel(8);
+            var result4 = problem.GetViaParallel(12);
+            var result5 = problem.GetViaParallel(16);
+            var result6 = await problem.GetViaTasks(4);
+            var result7 = await problem.GetViaTasks(8);
+            var result8 = await problem.GetViaTasks(12);
+            var result9 = await problem.GetViaTasks(16);
+            var resultA = problem.GetViaThreads(4);
+            var resultB = problem.GetViaThreads(8);
+            var resultC = problem.GetViaThreads(12);
+            var resultD = problem.GetViaThreads(16);
+
+
+            //  print results
+            Console.WriteLine($"Basic           :: time: { result1.ElapsedTime } ms \t| primes count: { result1.PrimesCount }");
+            Console.WriteLine($"Parallel (4)    :: time: { result2.ElapsedTime } ms \t| primes count: { result2.PrimesCount }");
+            Console.WriteLine($"Parallel (8)    :: time: { result3.ElapsedTime } ms \t| primes count: { result3.PrimesCount }");
+            Console.WriteLine($"Parallel (12)   :: time: { result4.ElapsedTime } ms \t| primes count: { result4.PrimesCount }");
+            Console.WriteLine($"Parallel (16)   :: time: { result5.ElapsedTime } ms \t| primes count: { result5.PrimesCount }");
+            Console.WriteLine($"Tasks (4)       :: time: { result6.ElapsedTime } ms \t| primes count: { result6.PrimesCount }");
+            Console.WriteLine($"Tasks (8)       :: time: { result7.ElapsedTime } ms \t| primes count: { result7.PrimesCount }");
+            Console.WriteLine($"Tasks (12)      :: time: { result8.ElapsedTime } ms \t| primes count: { result8.PrimesCount }");
+            Console.WriteLine($"Tasks (16)      :: time: { result9.ElapsedTime } ms \t| primes count: { result9.PrimesCount }");
+            Console.WriteLine($"Threads (4)     :: time: { resultA.ElapsedTime } ms \t| primes count: { resultA.PrimesCount }");
+            Console.WriteLine($"Threads (8)     :: time: { resultB.ElapsedTime } ms \t| primes count: { resultB.PrimesCount }");
+            Console.WriteLine($"Threads (12)    :: time: { resultC.ElapsedTime } ms \t| primes count: { resultC.PrimesCount }");
+            Console.WriteLine($"Threads (16)    :: time: { resultD.ElapsedTime } ms \t| primes count: { resultD.PrimesCount }");
+
+
+            //  defer
+            Console.Read();
+        }
+    }
+}
